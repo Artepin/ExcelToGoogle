83eed223aa29d11ad977c9e1965b7229948bc6d6
Wolverine <artemkozlov68@gmail.com>
2021-07-07 11:37:19 +0300

    update 2

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-
-import httplib2
+# Подключаем библиотеки
+import httplib2 
 import apiclient.discovery
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials	
 
-CREDENTIALS_FILE = 'woven-catwalk-319011-6951c37150c4.json'  # Имя файла с закрытым ключом, вы должны подставить свое
+CREDENTIALS_FILE = 'seraphic-effect-248407-7ac2c44ec709.json'  # Имя файла с закрытым ключом, вы должны подставить свое
 
 # Читаем ключи из файла
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
 
 httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
+
+spreadsheet = service.spreadsheets().create(body = {
+    'properties': {'title': 'Первый тестовый документ', 'locale': 'ru_RU'},
+    'sheets': [{'properties': {'sheetType': 'GRID',
+                               'sheetId': 0,
+                               'title': 'Лист номер один',
+                               'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
+}).execute()
+spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
+print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
